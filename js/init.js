47b9404c1680e9dfc5f14b54296f8299f048d261
Server1567 <chapijala@outlook.com>
2017-07-23 11:14:36 -0400

    My Website BETA

---
+++ js/init.js
@@ -0,0 +1,6 @@
+// Function for Run the travel in the website from the Welcome Page.
+
+function run() {
+	// Here I'll type anything more later. Not now. :7
+	Materialize.toast("Por ahora esto es todo lo que tengo", 3000, 'rounded');
+}
