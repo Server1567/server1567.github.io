c292dbd750c552220cdec007e146dad41119c62f
Server1567 <chapijala@outlook.com>
2017-07-31 11:37:01 -0400

    Project Complete, initalizing SEO

--- js/init.js
+++ js/init.js
@@ -1,3 +1,10 @@
+/*
+ * Description: © Server1567 Website, imagina, aprende y crea.
+ * Author: Junior Jimenez - Server1567
+ * Author URL: https://server1567.com (PENDING)
+ * Description: This is my Website Portfolio, you can hire me anytime.
+*/
+
 // Uso de fullPage.JS
 
 function down(){
